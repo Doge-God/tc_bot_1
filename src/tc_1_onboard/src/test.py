dcb7a5f8eb27f014d25cfcfed911ddd9b8926d0a
Futian <Futianzhou@hotmail.com>
2024-08-04 20:15:20 +1200

    Utterance detection

--- src/tc_1_onboard/src/test.py
+++ src/tc_1_onboard/src/test.py
@@ -13,8 +13,6 @@ from deepgram import (
 
 load_dotenv()
 
-# URL for the realtime streaming audio you would like to transcribe
-URL = "http://stream.live.vc.bbcmedia.co.uk/bbc_world_service"
 API_KEY = os.getenv("DEEPGRAM_KEY")
 
 ################################
@@ -23,6 +21,7 @@ API_KEY = os.getenv("DEEPGRAM_KEY")
 all_mic_data = []
 all_transcripts = []
 audio_queue = []
+current_sentence = ""
 
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
@@ -34,31 +33,10 @@ def mic_callback(input_data, frame_count, time_info, status_flag):
     audio_queue.append(input_data)
     return (input_data, pyaudio.paContinue)
 
-#  # Set up microphone if streaming from mic
-# async def microphone():
-#     audio = pyaudio.PyAudio()
-#     stream = audio.open(
-#         format=FORMAT,
-#         channels=CHANNELS,
-#         rate=RATE,
-#         input=True,
-#         frames_per_buffer=CHUNK,
-#         stream_callback=mic_callback,
-#     )
-
-#     stream.start_stream()
-
-#     global SAMPLE_SIZE
-#     SAMPLE_SIZE = audio.get_sample_size(FORMAT)
-
-#     while stream.is_active():
-#         await asyncio.sleep(0.1)
-
-#     stream.stop_stream()
-#     stream.close()
 
 
 def main():
+    global current_sentence
     try:
         # open microphone stream
         audio = pyaudio.PyAudio()
@@ -73,21 +51,26 @@ def main():
 
         stream.start_stream()
 
-        # STEP 1: Create a Deepgram client using the API key
+        # Create a Deepgram client using the API key
         deepgram = DeepgramClient(API_KEY)
 
-        # STEP 2: Create a websocket connection to Deepgram
+        # Create a websocket connection to Deepgram
         dg_connection = deepgram.listen.websocket.v("1")
 
-        # STEP 3: Define the event handlers for the connection
+        # Define the event handlers for the connection
         def on_message(self, result, **kwargs):
-
+            global current_sentence
             if result.speech_final:
                 sentence = result.channel.alternatives[0].transcript
 
                 if len(sentence) == 0:
                     return
-                print(f"speaker: {sentence}")
+                current_sentence += f" {sentence}"
+
+        def on_utterance_end(self, utterance_end,**kwargs):
+            global current_sentence
+            print(current_sentence)
+            current_sentence = ""
 
         def on_metadata(self, metadata, **kwargs):
             print(f"\n\n{metadata}\n\n")
@@ -95,10 +78,11 @@ def main():
         def on_error(self, error, **kwargs):
             print(f"\n\n{error}\n\n")
 
-        # STEP 4: Register the event handlers
+        # Register the event handlers
         dg_connection.on(LiveTranscriptionEvents.Transcript, on_message)
         dg_connection.on(LiveTranscriptionEvents.Metadata, on_metadata)
         dg_connection.on(LiveTranscriptionEvents.Error, on_error)
+        dg_connection.on(LiveTranscriptionEvents.UtteranceEnd, on_utterance_end)
 
         # STEP 5: Configure Deepgram options for live transcription
         options = LiveOptions(
@@ -106,7 +90,13 @@ def main():
             language="en-US", 
             smart_format=True,
             encoding="linear16",
-            sample_rate=RATE
+            sample_rate=RATE,
+            # VAD event settings
+            interim_results=True,
+            vad_events=True,
+            utterance_end_ms="1000",
+            # Time in milliseconds of silence to wait for before finalizing speech
+            endpointing=30
             )
         
         # STEP 6: Start the connection
@@ -114,27 +104,30 @@ def main():
 
         # STEP 7: Create a lock and a flag for thread synchronization
         lock_exit = threading.Lock()
+        lock_pause = threading.Lock()
         exit = False
+        pause = False
 
         # STEP 8: Define a thread that streams the audio and sends it to Deepgram
         def myThread():
-            # with httpx.stream("GET", URL) as r:
-            #     for data in r.iter_bytes():
-            #         lock_exit.acquire()
-            #         if exit:
-            #             break
-            #         lock_exit.release()
-
-            #         dg_connection.send(data)
             while True:
+                # get data from audio queue
                 if len(audio_queue) == 0:
                     continue
                 data = audio_queue.pop(0)
+                # lock exit flag and check for exit
                 lock_exit.acquire()
                 if exit:
                     break
                 lock_exit.release()
+                
+                # lock pause flag and check for pause
+                lock_pause.acquire()
+                if pause:
+                    continue
+                lock_pause.release()
 
+                # send data over stream
                 dg_connection.send(data)
 
 
