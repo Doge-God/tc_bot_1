a60116d47312cc5427abeeb2abf035496aef084a
FZ <Futianzhou@hotmail.com>
2024-09-03 23:02:35 +1200

    visual context implementation

--- src/tc_1/scripts/llm_manager_node.py
+++ src/tc_1/scripts/llm_manager_node.py
@@ -9,33 +9,37 @@ from std_msgs.msg import String
 import os
 from dotenv import load_dotenv
 from openai import OpenAI
-import subprocess
+from cv_bridge import CvBridge
+import base64
+import cv2
+from sensor_msgs.msg import CompressedImage, Image
+
 from tc_1.srv import SttControl
-from tc_1.srv import UpdateVisualPrompt, UpdateVisualPromptResponse
 from tc_1.srv import InjectUserInput, InjectUserInputResponse
 from tc_1.srv import InjectLLMResponse, InjectLLMResponseResponse
+from tc_1.srv import UpdateVisualContext, UpdateVisualContextResponse
 from collections import deque
 
 load_dotenv()
 
 OPEN_AI_KEY = os.getenv("OPEN_AI_KEY")
-INTERACTION_BASE_SYSTEM_PROMPT = "You are a robot. You talk like Hal9000. You give very brief responses. Omit any formatting in your response."
+INTERACTION_BASE_SYSTEM_PROMPT = "You are a robot, you can hear, see, and move around. You talk like Hal9000. You give very brief responses. Omit any formatting in your response."
 
-VISUAL_BASE_SYSTEM_PROMPT ="Describe what you see. Respond in second person as if you are prompting a LLM."
+VISUAL_BASE_SYSTEM_PROMPT ="Describe what you see. Do not use \"The image shows\"."
 visual_additional_system_prompt = ""
 visual_system_promp = {
     "role":"system",
     "content": f"{VISUAL_BASE_SYSTEM_PROMPT} {visual_additional_system_prompt}"
 }
 
-# logical_context_strings = []
 visual_context_string = ""
 interaction_system_prompt = {
     "role": "system",
-    "content": f"{INTERACTION_BASE_SYSTEM_PROMPT} {visual_context_string}"
+    "content": f"{INTERACTION_BASE_SYSTEM_PROMPT} This is what you see: {visual_context_string}"
 }
 message_log = deque([], maxlen=30)
 open_ai_client = OpenAI(api_key=OPEN_AI_KEY)
+bridge = CvBridge()
 
 def create_user_msg(msg:str):
     return {"role":"user","content":msg}
@@ -43,10 +47,27 @@ def create_user_msg(msg:str):
 def create_assist_msg(msg:str):
     return {"role":"assistant","content":msg}
 
+def create_image_msg(base64_img):
+    return {"role":"user","content":[
+        {
+            "type":"image_url",
+            "image_url": {
+                "url":f"data:image/jpeg;base64,{base64_img}"
+            }
+        }
+    ]}
+
 def create_system_prompt(msg:str):
     return {"role":"system", "content":msg}
 
+def get_gpt_response(messages):
+    global message_log, interaction_system_prompt
+    completion = open_ai_client.chat.completions.create(model="gpt-4o-mini", messages=messages)
+    gpt_msg = completion.choices[0].message.content.strip()
 
+    message_log.append(create_assist_msg(gpt_msg))
+
+    return gpt_msg
     
 
 def llm_manager():
@@ -56,17 +77,39 @@ def llm_manager():
     llm_sentence_pub = rospy.Publisher("llm_sentence",String, queue_size=5)
     stt_sentence_pub = rospy.Publisher('stt_sentence', String, queue_size=5)
 
-    def handle_update_visual_prompt(req):
-        global visual_additional_system_prompt
+    #========================================================================================
+    #=============  SERVICE HANDLERS ========================================================
+    #========================================================================================
 
-    # setup inject_user_input service
+    #### UPDATE VISUAL CONTEXT
+    def handle_update_visual_context(req):
+        global visual_context_string
+
+        base_prompt = "Describe what you see. Do not use \"The image shows\"."
+        extra_prompt = str(req.visualPrompt)
+        ros_img:CompressedImage = rospy.wait_for_message("/camera/rgb/image_raw/compressed",CompressedImage)
+        cv_img = bridge.compressed_imgmsg_to_cv2(ros_img, desired_encoding='passthrough')
+        _, buffer = cv2.imencode('.jpg', cv_img)
+        base64_img = base64.b64encode(buffer).decode('utf-8')
+
+        system_prompt = create_system_prompt(f"{base_prompt} {extra_prompt}")
+        image_message = create_image_msg(base64_img)
+
+        new_visual_context = get_gpt_response([system_prompt, image_message])
+        visual_context_string = new_visual_context
+
+        return UpdateVisualContextResponse(new_visual_context)
+    rospy.Service("update_visual_context", UpdateVisualContext, handle_update_visual_context)
+
+
+    #### INJECT UPSER INPUT
     def handle_inject_user_input(req):
         fake_user_input = str(req.fakeUserInput)
         stt_sentence_pub.publish(fake_user_input)
         return InjectUserInputResponse("success")
     rospy.Service("inject_user_input", InjectUserInput, handle_inject_user_input)
 
-    # setup inject_llm_response service
+    #### INJECT LLM RESPONSE
     def handle_inject_llm_response(req):
         global message_log
         fake_llm_response = str(req.fakeLLMResponse)
@@ -79,24 +122,12 @@ def llm_manager():
         llm_sentence_pub.publish(fake_llm_response)
         return InjectLLMResponseResponse("success")
     rospy.Service("inject_llm_response",InjectLLMResponse, handle_inject_llm_response)
+    #========================================================================================
 
+    
 
-    def get_gpt_response(messages):
-        global message_log, interaction_system_prompt
-        completion = open_ai_client.chat.completions.create(model="gpt-4o-mini", messages=messages)
-        gpt_msg = completion.choices[0].message.content.strip()
-
-        message_log.append(create_assist_msg(gpt_msg))
-        
-        # if (len(message_log) >= 25):
-        #     message_log.pop(0)
-        #     message_log.pop(0)
-        #     message_log.insert(0,system_prompt)
-
-        return gpt_msg
 
     def on_user_speech(data):
-
         try:
             set_stt_active = rospy.ServiceProxy("stt_control", SttControl)
             set_stt_active(False)
